3ec754288d3f01813bd190e9455f954f0e44f3cb
Shardul Khandekar <khandekarshardul@gmail.com>
2026-01-19 00:00:00 -0500

    changing creation logic for car wash

--- backend/services/washService.js
+++ backend/services/washService.js
@@ -1,6 +1,8 @@
 const db = require('../db/connection');
 const { createResponse } = require('../utils/response');
 
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 const washService = {
 
     // Create a new location
@@ -8,8 +10,28 @@ const washService = {
         return new Promise((resolve) => {
             const query = `INSERT INTO car_washes (owner_email, name, address, zipCode) VALUES (?, ?, ?, ?)`;
             db.run(query, [owner_email, name, address, zipCode], function(err) {
-                if (err) resolve(createResponse(false, null, err));
-                else resolve(createResponse(true, { id: this.lastID }));
+                if (err) {
+                    return resolve({ success: false, message: "Failed to create wash", error: err });
+                } else{
+                    // Get the ID of the newly created wash
+                    const washId = this.lastID;
+                    // Create default hours for the new wash
+                    const hoursQuery = `
+                        INSERT INTO business_hours (wash_id, day_of_week, open_time, close_time, is_closed)
+                        VALUES ${DAYS.map(() => "(?, ?, ?, ?, ?)").join(", ")}
+                    `;
+                    const hoursParams = [];
+                    DAYS.forEach(day => {
+                        hoursParams.push(washId, day, '09:00', '17:00', 0);
+                    });
+                    db.run(hoursQuery, hoursParams, (hourErr) => {
+                        if (hourErr) {
+                            return resolve({ success: false, message: "Wash created, but hours failed to initialize", error: hourErr });
+                        } else{
+                            resolve({ success: true, data: { id: washId } });
+                        }
+                    });
+                }
             });
         });
     },
